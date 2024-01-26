54b60b05dbae843ea31e2bc46253ad445bfc5742
shaniAbeywick <shaniabeywickrama294@gmail.com>
2024-01-26 18:16:35 +0530

    adding the changes to testimonial removing undefined code

--- app/components/Testimonial.tsx
+++ app/components/Testimonial.tsx
@@ -2,9 +2,7 @@
 import React from 'react'
 var $ = require("jquery");
 
-if (typeof window !== "undefined") {
-    window.$ = window.jQuery = require("jquery");
-}
+
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import dynamic from "next/dynamic";
