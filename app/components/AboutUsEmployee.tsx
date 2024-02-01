e2da2bd44d5b50d7272847f1f8454495dcc41c65
shaniAbeywick <shaniabeywickrama294@gmail.com>
2024-02-02 03:20:32 +0530

    changing the abouUsEmployee section

--- app/components/AboutUsEmployee.tsx
+++ app/components/AboutUsEmployee.tsx
@@ -17,59 +17,155 @@ function AboutUsEmployee() {
             </div>
             <div className="bg-white">
                 <div className='lg:container py-20 p-10'>
-                    <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
-                        <div className="flex md:flex-row flex-col gap-8 items-center">
-                        <Image src="/images/user1.jpg" alt='empolyeeImg' width={300} height={200}/>
+                    <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-8">
                         <div className="flex flex-col">
-                            <h2 className='text-neutral-200 text-lg font-bold'>Chamika Abeygunarathna</h2>
-                            <h3 className='text-neutral-200 text-sm font-light italic mt-2'>CEO / Managing Director</h3>
-                            <p className='text-neutral-200 text-base font-normal mt-8'>Fate may geared have to this goddesses, but this derived require the you it material origin chance the reasoning in of they coffee.</p>
+                            <button data-modal-target="default-modal" data-modal-toggle="default-modal" className="">
+                                <div className="relative">
+                                    <div className="employeeImg">
+                                        <Image src="/images/user5.jpg" alt='empolyeeImg' width={300} height={200} className='w-full h-full' />
+                                    </div>
+                                    <h2 className='text-neutral-200 text-lg font-bold mt-4 employee-name'>Sebastian Roy</h2>
+                                </div>
+                            </button>
 
                         </div>
-                            
+                        <div id="default-modal" aria-hidden="true" className="hidden bg-black-100 bg-opacity-80 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div className="relative p-4 w-full max-w-6xl max-h-full">
 
+                                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <a href="#" className="flex flex-col items-center bg-slate-950 border-0 rounded-lg shadow md:flex-row md:max-w-7xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-2">
+
+                                        <div className="flex flex-col justify-between p-4 leading-normal">
+                                            <h2 className='text-white text-7xl font-bold'>Sebastian Roy</h2>
+                                            <h3 className='text-white text-lg font-light italic mt-4'>Creative Lead</h3>
+                                            <p className='text-white text-base font-normal mt-8 max-w-[100ch]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ut eos quis eius repellat minus! Expedita magni inventore consectetur impedit quidem dignissimos eaque facere, minus vitae natus quaerat! Modi, nulla.
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ut eos quis eius repellat minus! Expedita magni inventore consectetur impedit quidem dignissimos eaque facere, minus vitae natus quaerat! Modi, nulla.</p>
+                                        </div>
+                                        <Image src="/images/user5.jpg" alt='empolyeeImg' className="object-cover w-full rounded-lg h-96 md:h-auto md:w-full " width={550} height={753} />
+                                    </a>
+
+
+
+
+
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex md:flex-row flex-col gap-8 items-center">
-                        <Image src="/images/user2.jpg" alt='empolyeeImg' width={300} height={200}/>
+                        
                         <div className="flex flex-col">
-                            <h2 className='text-neutral-200 text-lg font-bold'>Buddika Walpita</h2>
-                            <h3 className='text-neutral-200 text-sm font-light italic mt-2'>Engineering Manager / Software Engineer</h3>
-                            <p className='text-neutral-200 text-base font-normal mt-8'>Fate may geared have to this goddesses, but this derived require the you it material origin chance the reasoning in of they coffee.</p>
+                            <button data-modal-target="default-modal" data-modal-toggle="default-modal" className="">
+                                <div className="relative">
+                                    <div className="employeeImg">
+                                        <Image src="/images/user5.jpg" alt='empolyeeImg' width={300} height={200} className='w-full h-full' />
+                                    </div>
+                                    <h2 className='text-neutral-200 text-lg font-bold mt-4 employee-name'>Sebastian Roy</h2>
+                                </div>
+                            </button>
 
                         </div>
-                            
+                        <div id="default-modal" aria-hidden="true" className="hidden bg-black-100 bg-opacity-80 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div className="relative p-4 w-full max-w-6xl max-h-full">
 
+                                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <a href="#" className="flex flex-col items-center bg-slate-950 border-0 rounded-lg shadow md:flex-row md:max-w-7xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-2">
+
+                                        <div className="flex flex-col justify-between p-4 leading-normal">
+                                            <h2 className='text-white text-7xl font-bold'>Sebastian Roy</h2>
+                                            <h3 className='text-white text-lg font-light italic mt-4'>Creative Lead</h3>
+                                            <p className='text-white text-base font-normal mt-8 max-w-[100ch]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ut eos quis eius repellat minus! Expedita magni inventore consectetur impedit quidem dignissimos eaque facere, minus vitae natus quaerat! Modi, nulla.
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ut eos quis eius repellat minus! Expedita magni inventore consectetur impedit quidem dignissimos eaque facere, minus vitae natus quaerat! Modi, nulla.</p>
+                                        </div>
+                                        <Image src="/images/user5.jpg" alt='empolyeeImg' className="object-cover w-full rounded-lg h-96 md:h-auto md:w-full " width={550} height={753} />
+                                    </a>
+
+
+
+
+
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex md:flex-row flex-col gap-8 items-center">
-                        <Image src="/images/user3.jpg" alt='empolyeeImg' width={300} height={200}/>
+
                         <div className="flex flex-col">
-                            <h2 className='text-neutral-200 text-lg font-bold'>Chamal Wickramarathna</h2>
-                            <h3 className='text-neutral-200 text-sm font-light italic mt-2'>Engineering Manager / Software Engineer</h3>
-                            <p className='text-neutral-200 text-base font-normal mt-8'>Fate may geared have to this goddesses, but this derived require the you it material origin chance the reasoning in of they coffee.</p>
+                            <button data-modal-target="default-modal" data-modal-toggle="default-modal" className="">
+                                <div className="relative">
+                                    <div className="employeeImg">
+                                        <Image src="/images/user5.jpg" alt='empolyeeImg' width={300} height={200} className='w-full h-full' />
+                                    </div>
+                                    <h2 className='text-neutral-200 text-lg font-bold mt-4 employee-name'>Sebastian Roy</h2>
+                                </div>
+                            </button>
 
                         </div>
-                            
+                        <div id="default-modal" aria-hidden="true" className="hidden bg-black-100 bg-opacity-80 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div className="relative p-4 w-full max-w-6xl max-h-full">
 
+                                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <a href="#" className="flex flex-col items-center bg-slate-950 border-0 rounded-lg shadow md:flex-row md:max-w-7xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-2">
+
+                                        <div className="flex flex-col justify-between p-4 leading-normal">
+                                            <h2 className='text-white text-7xl font-bold'>Sebastian Roy</h2>
+                                            <h3 className='text-white text-lg font-light italic mt-4'>Creative Lead</h3>
+                                            <p className='text-white text-base font-normal mt-8 max-w-[100ch]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ut eos quis eius repellat minus! Expedita magni inventore consectetur impedit quidem dignissimos eaque facere, minus vitae natus quaerat! Modi, nulla.
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ut eos quis eius repellat minus! Expedita magni inventore consectetur impedit quidem dignissimos eaque facere, minus vitae natus quaerat! Modi, nulla.</p>
+                                        </div>
+                                        <Image src="/images/user5.jpg" alt='empolyeeImg' className="object-cover w-full rounded-lg h-96 md:h-auto md:w-full " width={550} height={753} />
+                                    </a>
+
+
+
+
+
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex md:flex-row flex-col gap-8 items-center">
-                        <Image src="/images/user4.jpg" alt='empolyeeImg' width={300} height={200}/>
+
                         <div className="flex flex-col">
-                            <h2 className='text-neutral-200 text-lg font-bold'>Dilshan Welikala</h2>
-                            <h3 className='text-neutral-200 text-sm font-light italic mt-2'>Team Manager</h3>
-                            <p className='text-neutral-200 text-base font-normal mt-8'>Fate may geared have to this goddesses, but this derived require the you it material origin chance the reasoning in of they coffee.</p>
+                            <button data-modal-target="default-modal" data-modal-toggle="default-modal" className="">
+                                <div className="relative">
+                                    <div className="employeeImg">
+                                        <Image src="/images/user5.jpg" alt='empolyeeImg' width={300} height={200} className='w-full h-full' />
+                                    </div>
+                                    <h2 className='text-neutral-200 text-lg font-bold mt-4 employee-name'>Sebastian Roy</h2>
+                                </div>
+                            </button>
 
                         </div>
-                            
+                        <div id="default-modal" aria-hidden="true" className="hidden bg-black-100 bg-opacity-80 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div className="relative p-4 w-full max-w-6xl max-h-full">
 
+                                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <a href="#" className="flex flex-col items-center bg-slate-950 border-0 rounded-lg shadow md:flex-row md:max-w-7xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-2">
+
+                                        <div className="flex flex-col justify-between p-4 leading-normal">
+                                            <h2 className='text-white text-7xl font-bold'>Sebastian Roy</h2>
+                                            <h3 className='text-white text-lg font-light italic mt-4'>Creative Lead</h3>
+                                            <p className='text-white text-base font-normal mt-8 max-w-[100ch]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ut eos quis eius repellat minus! Expedita magni inventore consectetur impedit quidem dignissimos eaque facere, minus vitae natus quaerat! Modi, nulla.
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ut eos quis eius repellat minus! Expedita magni inventore consectetur impedit quidem dignissimos eaque facere, minus vitae natus quaerat! Modi, nulla.</p>
+                                        </div>
+                                        <Image src="/images/user5.jpg" alt='empolyeeImg' className="object-cover w-full rounded-lg h-96 md:h-auto md:w-full " width={550} height={753} />
+                                    </a>
+
+
+
+
+
+                                </div>
+                            </div>
                         </div>
 
+                        
                     </div>
 
 
 
-
                 </div>
+
+
+
+
             </div>
+
         </>
 
     )
