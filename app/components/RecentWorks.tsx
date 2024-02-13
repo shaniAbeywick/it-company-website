e39951eaf552d5b3a35b87beb4dc219980f4d44d
shaniAbeywick <shaniabeywickrama294@gmail.com>
2024-02-13 23:33:49 +0530

    changing again the recent work image 1

--- app/components/RecentWorks.tsx
+++ app/components/RecentWorks.tsx
@@ -24,7 +24,7 @@ function RecentWorks() {
                 <div className="grid md:grid-cols-3 grid-cols-1 gap-8 justify-items-center">
                     <div className="relative w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden recentWorksCard hover:border-blue-300 hover:border-4">
                         <a href="https://www.infogate.lk/">
-                        <Image src="/images/slcfd.lk_.png" alt='recentWorkImg' width={744} height={400} className="absolute object-cover recentWorkImg" />
+                        <Image src="/images/infogate.lk.png" alt='recentWorkImg' width={744} height={400} className="absolute object-cover recentWorkImg" />
                         </a>
                     </div>
                     <div className="relative w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden recentWorksCard1 hover:border-blue-300 hover:border-4">
